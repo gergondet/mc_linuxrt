dd7070418f33ff2f86dde56e4bc5c6bbe31e7490
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-04-11 11:22:57 +0800

    Log the latency using mc_rtc

--- src/latency.cpp
+++ src/latency.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <thread>
 
+#include <mc_rtc/log/Logger.h>
+
 namespace app
 {
 
@@ -17,19 +19,28 @@ using time_point = std::chrono::time_point<clock>;
 struct LatencyMeasure
 {
   LatencyMeasure(uint64_t usec)
-  : usec_(usec), nMeasures_(1000000 / usec_)
+  : usec_(usec), nMeasures_(1000000 / usec_), logger_(mc_rtc::Logger::Policy::THREADED, ".", "")
   {
+    logger_.open("./latency-log.bin", 0.001);
+    logger_.addLogEntry("delay", [this]() { return delay_; });
+    logger_.addLogEntry("limit", [this]() { return 1.1 * usec_ / 1000; });
   }
 
   void run()
   {
+    timespec tv;
+    clock_gettime(CLOCK_REALTIME, &tv);
+    double prev_ = tv.tv_sec * 1000 + tv.tv_nsec * 1e-6;
     while(MC_LINUXRT_RUN)
     {
-      auto now = clock::now();
-      auto delay = std::chrono::duration_cast<std::chrono::microseconds>(now - prev_);
-      avg_ += delay.count();
-      max_ = std::max<double>(delay.count(), max_);
-      if(delay.count() > 1.1 * usec_)
+      //auto now = clock::now();
+      clock_gettime(CLOCK_REALTIME, &tv);
+      double now = tv.tv_sec * 1000 + tv.tv_nsec * 1e-6;
+      //auto delay = std::chrono::duration_cast<std::chrono::microseconds>(now - prev_);
+      delay_ = now - prev_;
+      avg_ += delay_;
+      max_ = std::max<double>(delay_, max_);
+      if(1000 * delay_ > 1.1 * usec_)
       {
         missed_ += 1;
       }
@@ -43,6 +54,7 @@ struct LatencyMeasure
         avg_ = 0;
         missed_ = 0;
       }
+      logger_.log();
       sched_yield();
     }
   }
@@ -51,9 +63,11 @@ struct LatencyMeasure
   unsigned int usec_ = 0;
   double avg_ = 0;
   double max_ = 0;
+  double delay_ = 0;
   size_t missed_ = 0;
   size_t iter_ = 0;
   size_t nMeasures_ = 1000;
+  mc_rtc::Logger logger_;
 };
 
 } // namespace app
